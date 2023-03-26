1947d4b005c4f4c4bb484d7ccdbd5b048b270acd
David Alvarado <78248845+Lualvarad0@users.noreply.github.com>
2023-03-25 21:26:53 -0500

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Prueba'),
+      dir: require('path').join(__dirname, './coverage/app-tarea'),
       subdir: '.',
       reporters: [
         { type: 'html' },
